b7d9c72b156f31fcc5bac7d348ba6b1282e10490
SAUZET-Leny <leny.sauzet@gmail.com>
2023-09-22 00:08:54 +0200

    Day9-end

--- Python/Day9/Hangman/main.py
+++ Python/Day9/Hangman/main.py
@@ -50,7 +50,6 @@ def ShowCorrectFrame(Sprites,index):
         else:
             Frame.set_alpha(0)  # Rendre l'image invisible
 
-
 # init
 pygame.init()
 
@@ -70,25 +69,6 @@ pygame.display.set_caption(rootTitle)
 # Event to change the frame
 CHANGE_IMAGE_EVENT = pygame.USEREVENT + 1
 pygame.time.set_timer(CHANGE_IMAGE_EVENT, 100)
-curIndex = 0
-
-# pick random word
-word = data.words[random.randint(0,len(data.words))-1]
-
-# Create a list of letters
-letters = []
-for letter in word:
-    letters.append((letter, False))
-
-# Show the first and last letters
-letters[0] = (letters[0][0], True)
-letters[-1] = (letters[-1][0], True)
-
-# show all spaces
-for i, (caractere, truth) in enumerate(letters):
-    if caractere == " ":
-        letters[i] = (caractere, True)
-
 
 # Sprite
 # sans 
@@ -98,114 +78,139 @@ SpriteSansDance = [pygame.image.load(f"assets\\sans-dance\\{index}.gif") for ind
 # sans shrug
 SpriteSansShrug = [pygame.image.load(f"assets\\sans-shrug\\{index}.gif") for index in range(0, 13)]
 
-
-# cursor
-# Chargez l'image du curseur personnalisé et redimensionnez-la
-new_cursor = pygame.image.load(Pathcursor)
-new_cursor = pygame.transform.scale(new_cursor, (35, 35))
-
-# Cachez le curseur par défaut de la souris
-pygame.mouse.set_visible(False)
-
-# Créez une surface pour afficher le curseur personnalisé
-cursor_surface = pygame.Surface((35, 35), pygame.SRCALPHA)
-cursor_surface.blit(new_cursor, (0, 0))
-
-
-letterUsed = []
-alert = "Enter a Letter :"
-
-a = 100
-b = 100
-amount = .25
-
-score = 10
-
 music = pygame.mixer.music.load(PathMusic)
 pygame.mixer.music.play(-1)
 pygame.mixer.Sound.play(pygame.mixer.Sound(PathStartSFX))
 
-win = False
-lose = False
+def win(word):
+    curIndex = 0
 
-# assets\\sans\\
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-
-        elif event.type == CHANGE_IMAGE_EVENT:
-            curIndex = (curIndex + 1) % len(SpriteSans)  # Passer à l'image suivante, revenir à 0 si la limite est atteinte
-
-        elif event.type == pygame.KEYDOWN:
-            # Vérifier si une touche de lettre a été appuyée
-            if event.key >= pygame.K_a and event.key <= pygame.K_z:
-                playerInput = chr(event.key)
-
-                if playerInput in letterUsed:
-                    alert = f"letter '{playerInput}' already used"
-                    pygame.mixer.Sound.play(pygame.mixer.Sound(PathMistakeSFX))
-
-                else:
-                    letterUsed.append(playerInput)
-                    letterFound = False
-                    findCount = 0
-                    for i, (caractere, truth) in enumerate(letters):
-                        if caractere == playerInput and not truth:
-                            letters[i] = (caractere, True)
-                            findCount +=1
-                            letterFound = True
-        
-                    if letterFound:
-                        alert = f"Found {data.numbers[findCount]} '{playerInput}'"
-                        pygame.mixer.Sound.play(pygame.mixer.Sound(PathGoodSFX))   
-
-                    else:
-                        score -= 1
-                        alert =f"No '{playerInput}' found"     
-                        pygame.mixer.Sound.play(pygame.mixer.Sound(PathBadSFX))   
-                currentWord = display_word(letters)
-            
-                if "_" not in currentWord:
-                    # displayWin(alert)
-                    pygame.mixer.Sound.play(pygame.mixer.Sound(PathWinSFX))
-
-                    # heart = pygame.image.load("assets\\heart.png")
-                    # root.blit(heart, (100, 100))
-                    win = True
-
-                    break
-
-            if score == 0:
-                pygame.mixer.Sound.play(pygame.mixer.Sound(PathLoseSFX))
-                lose = True
-
-
-        # Sans
-        # Contrôlez la visibilité des images
-        ShowCorrectFrame(SpriteSans,curIndex)
-        root.fill((0, 0, 0))
-        root.blit(SpriteSans[curIndex], (465, 90))
-        pygame.display.flip()
+    while True:
+        root.fill("black")
 
         # Title
         PLAY_TEXT = get_font(45).render("HANGMAN", True, "White")
         PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 40))
         root.blit(PLAY_TEXT, PLAY_RECT)
 
-        if win :
-            PLAY_TEXT = get_font(25).render("Vivtory !", True, "orange")
-            PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 570))
-            root.blit(PLAY_TEXT, PLAY_RECT)
-        elif lose :
-            PLAY_TEXT = get_font(25).render("You Lose !", True, "red")
-            PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 570))
-            root.blit(PLAY_TEXT, PLAY_RECT)
-        else:
-            # alert
-            PLAY_TEXT = get_font(25).render(alert, True, "white")
-            PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 570))
-            root.blit(PLAY_TEXT, PLAY_RECT)
+        # Sprite
+        ShowCorrectFrame(SpriteSansDance,curIndex)
+        root.blit(SpriteSansDance[curIndex], (390, 50))
+
+        PLAY_TEXT = get_font(25).render("Vivtory!", True, "orange")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 600))
+        root.blit(PLAY_TEXT, PLAY_RECT)
+
+        # Letter
+        PLAY_TEXT = get_font(70).render(word, True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 670))
+        root.blit(PLAY_TEXT, PLAY_RECT)
+
+        # TV
+        bg = pygame.image.load(PathOldTV)
+        bg = pygame.transform.scale(bg, (1280, 720))
+        root.blit(bg, (0, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                # sys.exit()
+            elif event.type == CHANGE_IMAGE_EVENT:
+                curIndex = (curIndex + 1) % len(SpriteSansDance)
+            elif event.type == pygame.KEYDOWN:
+                main() 
+
+        pygame.display.update()
+
+def lose(word):
+    curIndex = 0
+
+    while True:
+        root.fill("black")
+
+        # Title
+        PLAY_TEXT = get_font(45).render("HANGMAN", True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 40))
+        root.blit(PLAY_TEXT, PLAY_RECT)
+
+        # Sprite
+        ShowCorrectFrame(SpriteSansShrug,curIndex)
+        root.blit(SpriteSansShrug[curIndex], (385, 70))
+
+        PLAY_TEXT = get_font(25).render("you lose, the word was:", True, "red")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 600))
+        root.blit(PLAY_TEXT, PLAY_RECT)
+
+        # Letter
+        PLAY_TEXT = get_font(70).render(word, True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 670))
+        root.blit(PLAY_TEXT, PLAY_RECT)
+
+        # TV
+        bg = pygame.image.load(PathOldTV)
+        bg = pygame.transform.scale(bg, (1280, 720))
+        root.blit(bg, (0, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                # sys.exit()
+            elif event.type == CHANGE_IMAGE_EVENT:
+                curIndex = (curIndex + 1) % len(SpriteSansShrug)
+            elif event.type == pygame.KEYDOWN:
+                main() 
+
+
+        pygame.display.update()
+
+def main():
+    # pick random word
+    word = data.words[random.randint(0,len(data.words))-1]
+
+    # Create a list of letters
+    letters = []
+    for letter in word:
+        letters.append((letter, False))
+
+    # Show the first and last letters
+    letters[0] = (letters[0][0], True)
+    letters[-1] = (letters[-1][0], True)
+
+    # show all spaces
+    for i, (caractere, truth) in enumerate(letters):
+        if caractere == " ":
+            letters[i] = (caractere, True)
+            
+    # lerp init
+    a = 0
+    b = 100
+    amount = .25
+
+    # sprite index init
+    curIndex = 0
+
+    # score init
+    score = 10
+
+    letterUsed = []
+    alert = "Enter a Letter :"
+
+    while True:
+        root.fill("black")
+
+        # Title
+        PLAY_TEXT = get_font(45).render("HANGMAN", True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 40))
+        root.blit(PLAY_TEXT, PLAY_RECT)
+
+        # Sprite
+        ShowCorrectFrame(SpriteSans,curIndex)
+        root.blit(SpriteSans[curIndex], (465, 90))
+
+        # alert
+        PLAY_TEXT = get_font(25).render(alert, True, "white")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 570))
+        root.blit(PLAY_TEXT, PLAY_RECT)
 
         # HQ Bar
         b = (score*610)/10
@@ -218,18 +223,57 @@ while True:
         PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 670))
         root.blit(PLAY_TEXT, PLAY_RECT)
 
-
-        # Obtenez la position de la souris
-        pos = pygame.mouse.get_pos()
-
-        # Affichez le curseur personnalisé à la position de la souris
-        root.blit(cursor_surface, pos)
-        pygame.display.flip()
-
         # TV
         bg = pygame.image.load(PathOldTV)
         bg = pygame.transform.scale(bg, (1280, 720))
         root.blit(bg, (0, 0))
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                # sys.exit()
 
-    pygame.display.flip()
+            elif event.type == CHANGE_IMAGE_EVENT:
+                curIndex = (curIndex + 1) % len(SpriteSans)
+
+            elif event.type == pygame.KEYDOWN:
+                # Vérifier si une touche de lettre a été appuyée
+                if event.key >= pygame.K_a and event.key <= pygame.K_z:
+                    playerInput = chr(event.key)
+
+                    if playerInput in letterUsed:
+                        alert = f"letter '{playerInput}' already used"
+                        pygame.mixer.Sound.play(pygame.mixer.Sound(PathMistakeSFX))
+
+                    else:
+                        letterUsed.append(playerInput)
+                        letterFound = False
+                        findCount = 0
+                        for i, (caractere, truth) in enumerate(letters):
+                            if caractere == playerInput and not truth:
+                                letters[i] = (caractere, True)
+                                findCount +=1
+                                letterFound = True
+
+                        if letterFound:
+                            alert = f"Found {data.numbers[findCount]} '{playerInput}'"
+                            pygame.mixer.Sound.play(pygame.mixer.Sound(PathGoodSFX))   
+
+                        else:
+                            score -= 1
+                            alert =f"No '{playerInput}' found"     
+                            pygame.mixer.Sound.play(pygame.mixer.Sound(PathBadSFX))   
+                    currentWord = display_word(letters)
+
+                    if "_" not in currentWord:
+                        pygame.mixer.Sound.play(pygame.mixer.Sound(PathWinSFX))
+                        win(word)
+                        # break
+
+                if score == 0:
+                    pygame.mixer.Sound.play(pygame.mixer.Sound(PathLoseSFX))
+                    lose(word)
+                
+        pygame.display.update()
+
+main()
